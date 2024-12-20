4869b642fc58ed7cae83248f9446f0f9cbdcf133
Blaise Travis <blaisetravis@Blaises-iMac-2.local>
2024-12-19 20:48:36 -0600

    finish

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Us');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
